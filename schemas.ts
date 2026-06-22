5f54d5fb5ddca32ea1d6dda7bb7ed2a679e3349a
Kai Niebes <kai.niebes@outlook.com>
2026-06-22 16:06:55 +0200

    _id as any

--- schemas.ts
+++ schemas.ts
@@ -32,10 +32,9 @@ export const ProfileTypeEnumSchema = t.UnionEnum(['user', 'organization'], {
   description: 'Defines the type of a profile, which can be either "user" or "organization".',
 });
 
-const IdSchema = t
-  .Transform(t.String())
-  .Decode(v => v?.toString?.() ?? v)
-  .Encode(v => v?.toString?.() ?? v);
+const IdSchema = t.Any({
+  description: 'Unique identifier provided by MongoDB',
+});
 
 export const IDocumentSchema = t.Object(
   { _id: IdSchema },
